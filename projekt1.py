5030bb13ec8a1aa8bcc3bf61581da6211102ac10
Nattaliaa <nlorek7@interia.pl>
2022-12-10 13:04:52 +0100

    nowe_nowe

---
+++ projekt1.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+#os.mkdir("nowyfolder3")
+#time.sleep(3)
+#os.rename("nowyfolder3", "nowyfolder2")
+#time.sleep(3)
+#os.rmdir("nowyfolder2")
+
+#os.system('cmd c "cd C:\\Users\\vdi-student\\Desktop"')
+#os.system('cmd c "dir /s new.txt >> result.txt"')
+
+#my_string1 = 'mom'
+#my_string2 = 'dad'
+#sum1 = my_string1 + my_string2
+#print(sum1)
+
+#sum2 = 'my ' + my_string1 + ' and my ' + my_string2
+#print(sum2)
+
+#print(sum2[3:6])
+#print(sum2[14:17])
+
+import datetime
+today = datetime.date.today()
+d1 = today.strftime('%d/%m/%Y')
