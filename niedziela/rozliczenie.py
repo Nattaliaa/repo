2b9a3f3c3fb276cc1b3f03d481f18cc0dcc88023
Nattaliia <nlorek7@interia.pl>
2022-12-11 09:35:28 +0100

    Niedziela

--- niedziela/rozliczenie.py
+++ niedziela/rozliczenie.py
@@ -3,11 +3,13 @@ path = 'C:\\Users\\vdi-student\\Desktop\\rozliczenie.csv'
 mode = 'r'
 with open (path, mode) as plik:
     content = plik.readlines()
+#print(content[3])
 
-print(content)
 for i in range (len(content)):
+    #print(content[i])
     content[i] = content[i].split(',')
-#print(content)
+  #  print(content[i])
+    #print(content[2][2])
 
 #2.Obliczenie sredniej wyplaty
 
@@ -21,5 +23,5 @@ print(round(average,2))
 
 for i in range(1,len(content)):
     if content[i][3] == 'k' and content[i][4] == 't':
-        total += 1
+        total = total + 1
     print(total)
