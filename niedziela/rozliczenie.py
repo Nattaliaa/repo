f6546eb3356820e643dba56c7ec7683bfd84aaa3
Nattaliia <nlorek7@interia.pl>
2022-12-11 10:05:27 +0100

    Niedziela3

--- niedziela/rozliczenie.py
+++ niedziela/rozliczenie.py
@@ -3,12 +3,12 @@ path = 'C:\\Users\\vdi-student\\Desktop\\rozliczenie.csv'
 mode = 'r'
 with open (path, mode) as plik:
     content = plik.readlines()
-#print(content[3])
+print(content[3])
 
 for i in range (len(content)):
-    #print(content[i])
+    print(content[i])
     content[i] = content[i].split(',')
-  #  print(content[i])
+    print(content[i])
     #print(content[2][2])
 
 #2.Obliczenie sredniej wyplaty
@@ -22,7 +22,12 @@ print("Srednia wynagrodzen: " , (round(average,2)))
 
 #3.Ile kobiet jest na macierzynskim.
 
-#for i in range(1,len(content)):
- #   if content[i][3] == 'k' and content[i][4] == 't':
-  #      total = total + 1
-   # print(total)
+total = 0
+for i in range(1,len(content)):
+    print(content[i])
+    content[i][4] = content[i][4].replace('\n', '')
+    if content[i][4] == 't' and content[i][3] == 'k':
+        total = total + 1
+print(total)
+
+
